ac0858eb582a609058846e6c62f83a2eed4a5fb3
Adrian Röfer <aroefer@uni-bremen.de>
2019-10-30 13:34:50 +0100

    Added object pose tracker to demo. Rotation alignment does not work at the moment.

--- src/kineverse_experiment_world/tracking_node.py
+++ src/kineverse_experiment_world/tracking_node.py
@@ -3,23 +3,27 @@ from collections import namedtuple
 
 from multiprocessing import RLock
 
-from kineverse.gradients.gradient_math import spw,\
-                                              rotation3_axis_angle,\
-                                              frame3_axis_angle, \
-                                              frame3_quaternion, \
-                                              norm, \
-                                              rot_of, \
-                                              pos_of, \
-                                              vector3
-from kineverse.gradients.diff_logic    import get_diff_symbol
-from kineverse.model.paths             import Path
-from kineverse.motion.min_qp_builder   import SoftConstraint as SC,\
-                                              TypedQPBuilder as TQPB
-from kineverse.motion.integrator       import CommandIntegrator
-from kineverse.network.model_client    import ModelClient
+from kineverse.gradients.gradient_math      import spw,\
+                                                   rotation3_axis_angle,\
+                                                   frame3_axis_angle, \
+                                                   frame3_quaternion, \
+                                                   norm, \
+                                                   rot_of, \
+                                                   pos_of, \
+                                                   vector3, \
+                                                   Position
+from kineverse.gradients.diff_logic         import get_diff_symbol, erase_type
+from kineverse.model.paths                  import Path
+from kineverse.motion.min_qp_builder        import SoftConstraint as SC,\
+                                                   TypedQPBuilder as TQPB, \
+                                                   generate_controlled_values
+from kineverse.motion.integrator            import CommandIntegrator
+from kineverse.network.model_client         import ModelClient
+from kineverse.visualization.ros_visualizer import ROSVisualizer
+from kineverse.time_wrapper                 import Time
 
 from kineverse_experiment_world.msg import PoseStampedArray as PSAMsg
-from sensor_msgs.msg   import JointState  as JointStateMsg
+from sensor_msgs.msg                import JointState       as JointStateMsg
 
 TrackerEntry = namedtuple('TrackerEntry', ['pose', 'update_state', 'model_cb'])
 
@@ -35,7 +39,12 @@ class TrackerNode(object):
         self.tracked_poses = {}
         self.integrator    = None
         self.lock          = RLock()
+        self.soft_constraints = {}
+        self.joints        = set()
+        self.joint_aliases = {}
         
+        self.visualizer = ROSVisualizer('/tracker_vis', 'map')
+
         self.pub_js  = rospy.Publisher(js_topic,    JointStateMsg, queue_size=1)
         self.sub_obs = rospy.Subscriber(obs_topic, PSAMsg, self.cb_process_obs, queue_size=5)
 
@@ -48,7 +57,7 @@ class TrackerNode(object):
             start_tick = len(self.tracked_poses) == 0
 
             if model_path not in self.tracked_poses:
-                syms = [(Path(model_path) + (x,)).to_symbol() for x in ['ax', 'ay', 'az', 'x', 'y', 'z']]
+                syms = [Position(Path(model_path) + (x,)) for x in ['ax', 'ay', 'az', 'x', 'y', 'z']]
                 def rf(msg, state):
                     matrix = frame3_quaternion(msg.position.x, msg.position.y, msg.position.z, 
                                                msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
@@ -56,11 +65,12 @@ class TrackerNode(object):
                     state[syms[0]] = axis[0] * angle
                     state[syms[1]] = axis[1] * angle
                     state[syms[2]] = axis[2] * angle
-                    state[syms[3]] = o[0, 3]
-                    state[syms[4]] = o[1, 3]
-                    state[syms[5]] = o[2, 3]
+                    state[syms[3]] = msg.position.x
+                    state[syms[4]] = msg.position.y
+                    state[syms[5]] = msg.position.z
 
                 def process_model_update(data):
+                    print('Received model update for: {}'.format(model_path))
                     self._generate_pose_constraints(model_path, data)
 
                 axis = vector3(*syms[:3])
@@ -96,9 +106,17 @@ class TrackerNode(object):
         with self.lock:
             if self.integrator is not None:
                 self.integrator.run(self._integration_factor, self._iterations)
-                self._js_msg.header.stamp = rospy.Time.now()
-                self._js_msg.name, self._js_msg.position = zip(*[(str(Path(s)), v) for s, v in self.integrator.state.items()])
+                self._js_msg.header.stamp = Time.now()
+                self._js_msg.name, self._js_msg.position = zip(*[(n, self.integrator.state[s]) for s, n in self.joint_aliases.items()])
                 self.pub_js.publish(self._js_msg)
+
+                if self.visualizer is not None:
+                    poses = [t.pose.subs(self.integrator.state) for t in self.tracked_poses.values()]
+                    self.visualizer.begin_draw_cycle('obs_points')
+                    self.visualizer.draw_poses('obs_points', spw.eye(4), 0.1, 0.02, [p for p in poses if len(p.free_symbols) == 0])
+                    self.visualizer.render()
+
+                #print('---\n{}'.format('\n'.join(['{}: {}'.format(n, c.expr.subs(self.integrator.state)) for n, c in self.soft_constraints.items() if 'align rotation' == n[-14:]])))
             else:
                 print('Integrator does not exist')
 
@@ -115,49 +133,48 @@ class TrackerNode(object):
         if str_path in self.tracked_poses:
             te = self.tracked_poses[str_path]
 
-            axis, angle = axis_angle_from_matrix((rot_of(model).T * te.pose))
-            r_rot_control = axis * angle
+            self.joints |= model.pose.free_symbols
+            self.joint_aliases = {s: Path(erase_type(s))[-1] for s in self.joints}
+
+            axis = vector3(*[Position(Path(str_path) + (x,)) for x in ['ax', 'ay', 'az']])
+
+            #axis, angle = spw.axis_angle_from_matrix(rot_of(te.pose)) #spw.axis_angle_from_matrix((rot_of(model.pose).T * rot_of(te.pose)))
+            r_rot_control = axis #axis * angle
 
             hack = rotation3_axis_angle([0, 0, 1], 0.0001)
 
-            axis, angle = axis_angle_from_matrix(rot_of(model) * hack)
+            axis, angle = spw.axis_angle_from_matrix(rot_of(model.pose))# * hack)
             c_aa = (axis * angle)
 
-            self.soft_constraints['{} align rotation 0'.format(str_path)] =                                                     SC(r_rot_control[0],
-                                                       r_rot_control[0],
-                                                       1,
-                                                       c_aa[0])
-            self.soft_constraints['{} align rotation 1'.format(str_path)] =                                                     SC(r_rot_control[1],
-                                                       r_rot_control[1],
-                                                       1,
-                                                       c_aa[1])
-            self.soft_constraints['{} align rotation 2'.format(str_path)] =                                                     SC(r_rot_control[2],
-                                                       r_rot_control[2],
-                                                       1,
-                                                       c_aa[2])
+            r_dist = norm(r_rot_control - c_aa)
+
+            self.soft_constraints['{} align rotation'.format(str_path)] = SC(-r_dist, -r_dist, 1, r_dist)
+
+            # self.soft_constraints['{} align rotation 0'.format(str_path)] = SC(r_rot_control[0],
+            #                                            r_rot_control[0],
+            #                                            1,
+            #                                            c_aa[0])
+            # self.soft_constraints['{} align rotation 1'.format(str_path)] = SC(r_rot_control[1],
+            #                                            r_rot_control[1],
+            #                                            1,
+            #                                            c_aa[1])
+            # self.soft_constraints['{} align rotation 2'.format(str_path)] = SC(r_rot_control[2],
+            #                                            r_rot_control[2],
+            #                                            1,
+            #                                            c_aa[2])
         
-            dist = norm(pos_of(model) - pos_of(te.pose))
+            dist = norm(pos_of(model.pose) - pos_of(te.pose))
             self.soft_constraints['{} align position'.format(str_path)] = SC(-dist, -dist, 1, dist)
 
             self.generate_opt_problem()
 
 
     def generate_opt_problem(self):
-        joint_symbols = set(sum([list(p.free_symbols) for p in self.tracked_poses.values()]))
+        joint_symbols    = self.joints
         opt_symbols      = {get_diff_symbol(j) for j in joint_symbols}
         hard_constraints = self.km_client.get_constraints_by_symbols(joint_symbols | opt_symbols)
 
-        controlled_values = {}
-        to_remove = set()
-        for k, c in hard_constraints.items():
-            if type(c.expr) is spw.Symbol and c.expr in controlled_symbols:
-                controlled_values[str(c.expr)] = ControlledValue(c.lower, c.upper, c.expr, 0.01)
-                to_remove.add(k)
-
-        hard_constraints = {k: c for k, c in hard_constraints.items() if k not in to_remove}
-        for s in controlled_symbols:
-            if str(s) not in controlled_values:
-                controlled_values[str(s)] = ControlledValue(-1e9, 1e9, s, 0.01)
+        controlled_values, hard_constraints = generate_controlled_values(hard_constraints, opt_symbols)
 
         self.integrator = CommandIntegrator(TQPB(hard_constraints, self.soft_constraints, controlled_values))
         self.integrator.restart('Pose Tracking')
