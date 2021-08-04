82f87f3c4c5bfef2f261790a39a2e21157817f9e
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2021-08-04 17:05:22 +0200

    Nobilia model is almost there

--- scripts/nobilia_test.py
+++ scripts/nobilia_test.py
@@ -1,6 +1,7 @@
 import math
 import rospy
-
+import numpy as np
+import kineverse.gradients.gradient_math as gm
 
 from kineverse.model.geometry_model         import GeometryModel, Path
 from kineverse.visualization.bpb_visualizer import ROSBPBVisualizer
@@ -15,7 +16,7 @@ if __name__=='__main__':
   km  = GeometryModel()
   vis = ROSBPBVisualizer('kineverse/nobilia/vis', base_frame='world')
 
-  create_nobilia_shelf(km, Path('nobilia'))
+  debug = create_nobilia_shelf(km, Path('nobilia'))
 
   km.clean_structure()
   km.dispatch_events()
@@ -27,9 +28,28 @@ if __name__=='__main__':
   world = km.get_active_geometry({shelf_pos})
 
   start = rospy.Time.now()
+
+  max_length_v = 0
+
   while not rospy.is_shutdown():
-    world.update_world({shelf_pos: (math.cos((rospy.Time.now() - start).to_sec()) + 1) * 0.5 * shelf.joints['hinge'].limit_upper})
-    vis.begin_draw_cycle('world')
+    state_range = shelf.joints['hinge'].limit_upper - shelf.joints['hinge'].limit_lower
+    state = {shelf_pos: shelf.joints['hinge'].limit_lower + (math.cos((rospy.Time.now() - start).to_sec()) + 1) * 0.5 * state_range }
+    world.update_world(state)
+    vis.begin_draw_cycle('world', 'poses', 'vectors')
     vis.draw_world('world', world)
+    vis.draw_poses('poses', np.eye(4), 0.1, 0.01, [gm.subs(p, state) for p in debug.poses])
+
+    for point, vector in debug.vectors:
+      vis.draw_vector('vectors', gm.subs(point, state), 
+                                 gm.subs(vector, state))
+
     vis.render()
+    
+    for name, expr in debug.expressions.items():
+      print(f'{name}: {gm.subs(expr, state)}')
+
+    max_length_v = max(max_length_v, gm.subs(debug.expressions['length_v'], state))
+
     rospy.sleep(rospy.Duration(1 / 50))
+
+  print(f'max length v: {max_length_v}')
--- src/kineverse_experiment_world/nobilia_shelf.py
+++ src/kineverse_experiment_world/nobilia_shelf.py
@@ -2,6 +2,7 @@ import math
 
 import kineverse.gradients.gradient_math as gm
 
+from collections import namedtuple
 from kineverse.model.paths          import Path, CPath
 from kineverse.model.geometry_model import ArticulatedObject, \
                                            Box, Mesh, Cylinder, Sphere, \
@@ -9,6 +10,22 @@ from kineverse.model.geometry_model import ArticulatedObject, \
 from kineverse.operations.basic_operations import CreateValue, \
                                                   ExecFunction
 from kineverse.operations.urdf_operations  import RevoluteJoint
+
+
+NobiliaDebug = namedtuple('NobiliaDebug', ['poses', 'vectors', 'expressions'])
+
+def inner_triangle_angle(a, b, c):
+  """Given the lengths of a triangle's sides, calculates the angle opposite of side c
+  
+  Args:
+      a (TYPE): Length of side a
+      b (TYPE): Length of side a
+      c (TYPE): Length of side a
+  
+  Returns:
+      TYPE: Angle opposite of side c
+  """
+  return gm.acos((a**2 + b**2 - c**2) / (2 * a * b))
 
 
 def create_nobilia_shelf(km, prefix, origin_pose=gm.eye(4), parent_path=Path('world')):
@@ -52,13 +69,13 @@ def create_nobilia_shelf(km, prefix, origin_pose=gm.eye(4), parent_path=Path('wo
                                                   collision=dict(enumerate(shelf_geom)))
 
     geom_panel_top = Box(l_prefix + ('panel_top',), gm.eye(4), 
-                                                    gm.vector3(wall_width, 
+                                                    gm.vector3(shelf_height * 0.5 - 0.005, 
                                                                shelf_width - 0.005,
-                                                               shelf_height * 0.5 - 0.005))
+                                                               wall_width))
     geom_panel_bottom = Box(l_prefix + ('panel_bottom',), gm.eye(4), 
-                                                          gm.vector3(wall_width, 
-                                                                     shelf_width - 0.005,
-                                                                     shelf_height * 0.5 - 0.005))
+                                                          gm.vector3(shelf_height * 0.5 - 0.005, 
+                                                               shelf_width - 0.005,
+                                                               wall_width))
     
     # Sketch of mechanism
     #                 
@@ -80,52 +97,72 @@ def create_nobilia_shelf(km, prefix, origin_pose=gm.eye(4), parent_path=Path('wo
     #   d -> delta_1 + delta_2 = delta
     #
 
+    opening_position = gm.Position(prefix + ('door',))
+
     # Calibration results
     #
     # Solution top hinge: cost = 0.03709980624159568 [ 0.08762252 -0.01433833  0.2858676   0.00871125]
     # Solution bottom hinge: cost = 0.025004236048128934 [ 0.1072496  -0.01232362  0.27271013  0.00489996]
 
-    opening_position = gm.Position(prefix + ('door',))
+    # Added 180 deg rotation due to -x being the forward facing side in this model
+    top_hinge_in_body_marker           = gm.translation3(0.08762252, 0, -0.01433833)
+    top_panel_marker_in_top_hinge      = gm.translation3(0.2858676, 0, 0.00871125)
+    front_hinge_in_top_panel_maker     = gm.translation3(0.1072496, 0, -0.01232362)
+    bottom_panel_marker_in_front_hinge = gm.translation3(0.27271013, 0, 0.00489996)
 
     # Top hinge - Data taken from observation
-    body_marker_in_body = gm.translation3(0.5 * shelf_body_depth - 0.062, -0.5 * shelf_width + 0.078, 0.5 * shelf_height)
-    point_a  = gm.dot(gm.inverse_frame(body_marker_in_body), gm.point3(0.08762252, 0, -0.01433833))
-    # Hinge lift arm
-    point_d  = point_a + gm.vector3(-0.09, 0, -0.16)
-    point_b  = 
-    length_z = gm.norm(point_a - point_d)
-    
-    # Zero alpha along the vertical axis
-    alpha    = gm.acos(gm.dot_product(point_d - point_a, gm.vector3(0, 0, -1)) / gm.norm(point_d - point_a)) + opening_position
-
-    length_t = 0.37
-    length_b = 0.16
-    length_l = 0.38
-
-    gamma_1  = gm.atan((length_t - length_z) / (length_t - length_z) * gm.tan(0.5 * math.pi * alpha)) - 0.5 * (math.pi - alpha)
-    length_v = (length_t * gm.sin(alpha) / gm.sin(gamma_1))
-    gamma_2  = gm.acos((length_b**2 + length_v**2 - length_l**2) / (2 * length_b * length_v))
-
-    gamma = gamma_1 + gamma_2
-
+    body_marker_in_body = gm.dot(gm.rotation3_axis_angle(gm.vector3(0, 0, 1), math.pi), 
+                                 gm.translation3(0.5 * shelf_body_depth - 0.062, -0.5 * shelf_width + 0.078, 0.5 * shelf_height))
     top_panel_marker_in_top_panel = gm.translation3( geom_panel_top.scale[0] * 0.5 - 0.062,
                                                     -geom_panel_top.scale[1] * 0.5 + 0.062,
                                                      geom_panel_top.scale[2] * 0.5)
+    bottom_panel_marker_in_bottom_panel = gm.translation3( geom_panel_bottom.scale[0] * 0.5 - 0.062,
+                                                          -geom_panel_bottom.scale[1] * 0.5 + 0.062,
+                                                           geom_panel_bottom.scale[2] * 0.5)
+    
+    top_hinge_in_body        = gm.dot(body_marker_in_body, top_hinge_in_body_marker)
+    top_panel_in_top_hinge   = gm.dot(top_panel_marker_in_top_hinge, gm.inverse_frame(top_panel_marker_in_top_panel))
+    front_hinge_in_top_panel = gm.dot(top_panel_marker_in_top_panel, front_hinge_in_top_panel_maker)
+    bottom_panel_in_front_hinge = gm.dot(bottom_panel_marker_in_front_hinge, gm.inverse_frame(bottom_panel_marker_in_bottom_panel))
 
-    tf_top_panel = gm.dot(gm.translation3(point_a[0], point_a[1], point_a[2]),
-                          gm.rotation3_axis_angle(gm.vector3(0, 1, 0), opening_position + math.pi * 0.5),
-                          gm.dot(gm.translation3(0.27271013, 0, 0.00489996), gm.inverse_frame(top_panel_marker_in_top_panel)),
-                          gm.rotation3_axis_angle(gm.vector3(0, 1, 0), math.pi * 0.5))
+    top_panel_in_body   = gm.dot(top_hinge_in_body,  # Translation hinge to body frame
+                                 gm.rotation3_axis_angle(gm.vector3(0, 1, 0), opening_position + 0.5 * math.pi), # Hinge around y
+                                 top_panel_in_top_hinge)
+    front_hinge_in_body = gm.dot(top_panel_in_body, front_hinge_in_top_panel)
 
-    rb_panel_top = RigidBody(l_prefix + ('body',), gm.dot(rb_body.pose, tf_top_panel),
-                                                   tf_top_panel,
+    # Point a in body reference frame
+    point_a  = gm.dot(gm.diag(1, 0, 1, 1), gm.pos_of(top_hinge_in_body))
+    # Point b in top panel reference frame
+    point_b  = gm.dot(gm.diag(1, 0, 1, 1), gm.pos_of(front_hinge_in_body))
+    # Hinge lift arm in body reference frame
+    point_d  = point_a + gm.vector3(0.09, 0, -0.16)
+    length_z = gm.norm(point_a - point_d)
+    
+    # Zero alpha along the vertical axis
+    vec_a_to_d = gm.dot(point_d - point_a)
+    alpha      = math.atan2(vec_a_to_d[0], -vec_a_to_d[2]) + opening_position
+
+    vec_a_to_b = point_b - point_a
+    length_t = gm.norm(vec_a_to_b)
+    length_b = 0.16
+    length_l = 0.38
+
+    vec_b_to_d = point_d - point_b
+    length_v = gm.norm(vec_b_to_d)
+    gamma_1  = inner_triangle_angle(length_t, length_v, length_z)
+    gamma_2  = inner_triangle_angle(length_b, length_v, length_l)
+
+    gamma = gamma_1 + gamma_2
+
+    rb_panel_top = RigidBody(l_prefix + ('body',), gm.dot(rb_body.pose, top_panel_in_body),
+                                                   top_panel_in_body,
                                                    geometry={0: geom_panel_top},
                                                    collision={0: geom_panel_top})
 
     # old offset: 0.5 * geom_panel_top.scale[2] + 0.03
-    tf_bottom_panel = gm.dot(gm.translation3(0, 0, length_t),
+    tf_bottom_panel = gm.dot(front_hinge_in_top_panel,
                              gm.rotation3_axis_angle(gm.vector3(0, 1, 0), gamma),
-                             gm.translation3(0, 0, 0.5 * geom_panel_bottom.scale[2] - 0.03))
+                             bottom_panel_in_front_hinge)
 
     rb_panel_bottom = RigidBody(l_prefix + ('panel_top',), gm.dot(rb_panel_top.pose, tf_bottom_panel),
                                                            tf_bottom_panel,
@@ -142,7 +179,27 @@ def create_nobilia_shelf(km, prefix, origin_pose=gm.eye(4), parent_path=Path('wo
                                                                      opening_position,
                                                                      gm.vector3(0, 1, 0),
                                                                      gm.eye(4),
-                                                                     0,
-                                                                     2.2))
+                                                                     -2.2,
+                                                                     0))
 
-
+    return NobiliaDebug([top_hinge_in_body, 
+                         gm.dot(top_hinge_in_body, 
+                               gm.rotation3_axis_angle(gm.vector3(0, 1, 0), opening_position +  0.5 * math.pi), 
+                               top_panel_in_top_hinge,
+                               front_hinge_in_top_panel),
+                        body_marker_in_body,
+                        gm.dot(rb_panel_top.pose, top_panel_marker_in_top_panel),
+                        gm.dot(rb_panel_bottom.pose, bottom_panel_marker_in_bottom_panel)],
+                        [(point_a, vec_a_to_d),
+                         (point_a, vec_a_to_b),
+                         (point_b, vec_b_to_d)],
+                        {'gamma_1': gamma_1, 
+                         'gamma_2': gamma_2,
+                         'gamma_1 check_dot': gamma_1 - gm.acos(gm.dot_product(-vec_a_to_b / gm.norm(vec_a_to_b), vec_b_to_d / gm.norm(vec_b_to_d))),
+                         'gamma_1 check_cos': gamma_1 - inner_triangle_angle(gm.norm(vec_a_to_b), gm.norm(vec_b_to_d), gm.norm(vec_a_to_d)),
+                         'length_v': length_v,
+                         'length_b': length_b,
+                         'length_l': length_l,
+                         'position': opening_position,
+                         'alpha': alpha
+                         })
