b9015344824bd1d5aa7008588d02d5a415ea02eb
Adrian Röfer <aroefer@uni-bremen.de>
2021-08-09 20:21:41 +0200

    Better initial estimate of EKF tracker. However, in nobilia scenario non-invertability of S_t matrix in Kalman model persists.

--- scripts/kineverse_ekf_ar_tracker.py
+++ scripts/kineverse_ekf_ar_tracker.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python3
 import rospy
 import kineverse.gradients.gradient_math as gm
 import tf2_ros
+import numpy  as np
+import pandas as pd
 
-from geometry_msgs.msg import PoseStamped as PoseStampedMsg
-
-from kineverse.model.geometry_model import GeometryModel, Path
+from kineverse.model.geometry_model import GeometryModel, Path, Frame
 from kineverse_msgs.msg             import ValueMap as ValueMapMsg
+from kineverse.ros.tf_publisher     import ModelTFBroadcaster_URDF
+from kineverse.visualization.ros_visualizer import ROSVisualizer
 
 from kineverse_experiment_world.ekf_model import EKFModel
 from kineverse_experiment_world.nobilia_shelf import create_nobilia_shelf
+from kineverse_experiment_world.msg import TransformStampedArray as TransformStampedArrayMsg
 
 
 def np_frame3_quaternion(x, y, z, qx, qy, qz, qw):
     a  = [qx, qy, qz, qw]
-    mf = GM if max([type(v) == GC for v in a]) else cm.Matrix
+    mf = np.array
     qx2 = qx * qx
     qy2 = qy * qy
     qz2 = qz * qz
@@ -27,7 +31,7 @@ def np_frame3_quaternion(x, y, z, qx, qy, qz, qw):
 class Kineverse6DEKFTracker(object):
     def __init__(self, km, paths_observables,
                        transition_rules=None, 
-                       noise_estimation_observations=100, estimate_init_steps=10):
+                       noise_estimation_observations=100, estimate_init_steps=50):
         """Initializes the tracker to estimate variables based 
            on the given paths. The paths are assumed to point to
            6D poses.
@@ -39,6 +43,7 @@ class Kineverse6DEKFTracker(object):
             estimate_init_steps (int): Number of steps used to initialize the estimate of an ekf
         """
         poses = {p: km.get_data(p) for p in paths_observables}
+        poses = {str(p): pose.pose if isinstance(pose, Frame) else pose for p, pose in poses.items()}
 
 
         # Identify tracking pools. All poses within one pool share at least one DoF
@@ -78,7 +83,8 @@ class Kineverse6DEKFTracker(object):
         self.observation_names = [str(p) for p in paths_observables]
         self.controls          = set(sum([e.ordered_controls for e in self.ekfs], []))
 
-        self.estimates = [None] * len(self.ekfs)
+        self.estimates   = [None] * len(self.ekfs)
+        self.obs_counter = 0
 
         self._n_estimation_observations = noise_estimation_observations
         self._estimation_observations   = []
@@ -108,6 +114,8 @@ class Kineverse6DEKFTracker(object):
             if not self._ekfs_initialized:
                 self.compute_observation_covariance(self._estimation_observations)
 
+            self.obs_counter += 1
+
             for x, ekf in enumerate(self.ekfs):
                 # Currently only using a single estimate
                 # Proper particle filtering with a clever initialization
@@ -118,12 +126,46 @@ class Kineverse6DEKFTracker(object):
                     self.estimates[x] = ekf.spawn_particle()
                     estimate = self.estimates[x]
 
-                    for _ in range(self.estimate_init_steps):
+                    obs_vector = ekf.gen_obs_vector(observation)
+                    # obs_delta = obs_vector.reshape((len(obs_vector), 1)) - ekf.h_fn.call2(estimate.state)
+                    # df = pd.DataFrame(data=np.hstack((ekf.h_prime_fn.call2(estimate.state), obs_delta)), 
+                    #                   index=[str(l) for l in ekf.obs_labels],
+                    #                   columns=[str(c) for c in ekf.ordered_controls] + ['obs_delta'])
+                    # df.to_csv(f'ekf_{x}_preinit.csv')
+
+                    # deltas = []
+
+                    last_error = 1000
+                    while True:
                         h_prime   = ekf.h_prime_fn.call2(estimate.state)
                         obs_delta = obs_vector.reshape((len(obs_vector), 1)) - ekf.h_fn.call2(estimate.state)
-                        estimate.state += (h_prime.T.dot(obs_delta) * 0.5).reshape(estimate.state.shape)
+
+                        jjt   = h_prime.dot(h_prime.T)
+                        jjt_e = jjt.dot(obs_delta)
+                        alpha_numerator = obs_delta.T.dot(jjt_e)
+                        alpha = np.abs(alpha_numerator) / np.abs(jjt_e.T.dot(jjt_e))
+                        # print(x, alpha, obs_delta.T.dot(jjt_e), jjt_e.T.dot(jjt_e), np.linalg.cond(jjt))
+
+                        estimate.state += (h_prime.T.dot(obs_delta) * alpha).reshape(estimate.state.shape)
+                        error_norm = np.sqrt((obs_delta**2).sum())
+                        # deltas.append(np.hstack(([[error_norm, last_error - error_norm]], obs_delta.T)))
+                        if last_error - error_norm < 1e-6:
+                            break
+
+                        last_error = error_norm
+
+
+                    # pd.DataFrame(data=np.vstack(deltas), columns=['error_norm', 'error_delta'] + [str(l) for l in ekf.obs_labels]).to_csv(f'ekf_{x}_error_progression.csv')
+
+                    # obs_vector = ekf.gen_obs_vector(observation)
+                    # obs_delta = obs_vector.reshape((len(obs_vector), 1)) - ekf.h_fn.call2(estimate.state)
+                    # df = pd.DataFrame(data=np.hstack((ekf.h_prime_fn.call2(estimate.state), obs_delta)), 
+                    #                   index=[str(l) for l in ekf.obs_labels],
+                    #                   columns=[str(c) for c in ekf.ordered_controls] + ['obs_delta'])
+                    # df.to_csv(f'ekf_{x}_postinit.csv')
                 else:
                     estimate    = self.estimates[x]
+                    print(f'EKF {x} cov:\n{estimate.cov}')
                     estimate.state, estimate.cov = ekf.predict(estimate.state.flatten(), 
                                                                estimate.cov, 
                                                                ekf.gen_control_vector(control), dt=0.05)
@@ -135,41 +177,55 @@ class Kineverse6DEKFTracker(object):
     def get_estimated_state(self):
         if self._ekfs_initialized is None:
             return None
-        return dict(sum([zip(ekf.ordered_vars, estimate.state.flatten()) for ekf, estimate 
-                                                                          in zip(self.ekfs, self.estimates) if estimate is not None], []))
+        return dict(sum([list(zip(ekf.ordered_vars, estimate.state.flatten())) for ekf, estimate 
+                                                                               in zip(self.ekfs, self.estimates) if estimate is not None], []))
 
 class ROSEKFManager(object):
-    def __init__(self, tracker : Kineverse6DEKFTracker):
+    def __init__(self, tracker : Kineverse6DEKFTracker, 
+                       model=None, model_path=None, urdf_param='/ekf_description_check'):
         self.tracker = tracker 
         self.last_observation = {}
-        self.last_control     = None
         self.last_update      = None
+        self.last_control     = {s: 0.0 for s in self.tracker.controls}
+        self.str_controls     = {str(s) for s in self.tracker.controls}
+
+        self.vis = ROSVisualizer('~vis', 'world')
+
+        if model is not None:
+            self.broadcaster = ModelTFBroadcaster_URDF(urdf_param, model_path, model, Path('ekf'))
+        else:
+            self.broadcaster = None
 
         self.pub_state        = rospy.Publisher('~state_estimate', ValueMapMsg, queue_size=1, tcp_nodelay=True)
-        self.sub_observations = rospy.Subscriber('~observations', PoseStampedMsg, callback=self.cb_obs, queue_size=1)
+        self.sub_observations = rospy.Subscriber('~observations', TransformStampedArrayMsg, callback=self.cb_obs, queue_size=1)
         self.sub_controls     = rospy.Subscriber('~controls',        ValueMapMsg, callback=self.cb_controls, queue_size=1)
 
-    def cb_obs(self, pose_stamped_msg):
-        matrix = np_frame3_quaternion(pose_stamped_msg.pose.position.x, 
-                                      pose_stamped_msg.pose.position.y, 
-                                      pose_stamped_msg.pose.position.z,
-                                      pose_stamped_msg.pose.orientation.x,
-                                      pose_stamped_msg.pose.orientation.y,
-                                      pose_stamped_msg.pose.orientation.z,
-                                      pose_stamped_msg.pose.orientation.w)
-        # Yes, I am abusing the message type. Sue me
-        self.last_observation[pose_stamped_msg.header.frame_id] = matrix
-        try_update()
+    def cb_obs(self, transform_stamped_array_msg):
+        for trans in transform_stamped_array_msg.transforms:
+            matrix = np_frame3_quaternion(trans.transform.translation.x, 
+                                          trans.transform.translation.y, 
+                                          trans.transform.translation.z,
+                                          trans.transform.rotation.x,
+                                          trans.transform.rotation.y,
+                                          trans.transform.rotation.z,
+                                          trans.transform.rotation.w)
+            self.last_observation[trans.child_frame_id] = matrix
+
+        self.vis.begin_draw_cycle('observations')
+        self.vis.draw_poses('observations', np.eye(4), 0.2, 0.01, self.last_observation.values())
+        self.vis.render('observations')
+        self.try_update()
 
     def cb_controls(self, map_message):
-        self.last_control = {gm.Symbol(str_symbol): v in zip(map_message.symbol, map_message.value)}
-        try_update()
+        self.last_control = {gm.Symbol(str_symbol): v for str_symbol, v in zip(map_message.symbol, map_message.value) 
+                                                      if str_symbol in self.str_controls}
+        self.try_update()
 
     def try_update(self):
         if min(p in self.last_observation for p in self.tracker.observation_names) is False:
             return
 
-        if min(s in self.last_control for s in self.tracker.controls) is False:
+        if len(self.tracker.controls) > 0 and min(s in self.last_control for s in self.tracker.controls) is False:
             return
 
         now = rospy.Time.now()
@@ -177,14 +233,24 @@ class ROSEKFManager(object):
 
         self.tracker.process_update(self.last_observation, self.last_control, dt)
 
+        est_state = self.tracker.get_estimated_state()
+        if len(est_state) == 0:
+            return
+
         state_msg = ValueMapMsg()
         state_msg.header.stamp = now
-        state_msg.symbol, state_msg.value = zip(*self.tracker.get_estimated_state().items())
+        state_msg.symbol, state_msg.value = zip(*est_state.items())
         state_msg.symbol = [str(s) for s in state_msg.symbol]
         self.pub_state.publish(state_msg)
 
-        self.last_control = {}
+        print('Performed update of estimate and published it.')
+
+        # self.last_control = {}
         self.last_observation = {}
+        
+        if self.broadcaster is not None:
+            self.broadcaster.update_state(self.tracker.get_estimated_state())
+            self.broadcaster.publish_state()
 
 
 if __name__ == '__main__':
@@ -204,9 +270,11 @@ if __name__ == '__main__':
     shelf   = km.get_data('nobilia')
     tracker = Kineverse6DEKFTracker(km, 
                                     [Path(f'nobilia/markers/{name}') for name in shelf.markers.keys()], 
-                                    {x: x for x in gm.free_symbols(origin_pose)})
+                                    #{x: x for x in gm.free_symbols(origin_pose)},
+                                    noise_estimation_observations=20,
+                                    estimate_init_steps=1000)
 
-    node = ROSEKFManager(tracker)
+    node = ROSEKFManager(tracker, shelf, Path('nobilia'))
 
     reference_frame = rospy.get_param('~reference_frame', 'world')
     grab_from_tf    = rospy.get_param('~grab_from_tf', False)
