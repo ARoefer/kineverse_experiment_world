0b1a057790e14180a7aca959375d98e141f51a9d
Adrian Röfer <aroefer@uni-bremen.de>
2021-08-15 19:15:17 +0200

    Update nobilia kinematic sim and launch files

--- scripts/nobilia_kinematic_sim.py
+++ scripts/nobilia_kinematic_sim.py
@@ -3,23 +3,41 @@ import rospy
 import kineverse.gradients.gradient_math as gm
 
 from kineverse.model.geometry_model import GeometryModel, Path
-from kineverse_tools.kinematic_sim import KineverseKinematicSim
+from kineverse_tools.kinematic_sim  import KineverseKinematicSim
+
+from kineverse_msgs.msg import ValueMap as ValueMapMsg
 
 from kineverse_experiment_world.nobilia_shelf import create_nobilia_shelf
 
 if __name__ == '__main__':
-	rospy.init_node('nobilia_kinematic_sim')
+    rospy.init_node('nobilia_kinematic_sim')
 
-	km = GeometryModel()
+    root_transform = rospy.get_param('~root_transform', gm.eye(4))
+    if root_transform is not None and not gm.is_matrix(root_transform):
+        if type(root_transform) is not list and type(root_transform) is not tuple:
+            print(f'root_transform parameter is expected to be a list, but is {type(root_transform)}')
+            exit(1)
+        if len(root_transform) == 6:
+            root_transform = gm.frame3_rpy(root_transform[3],
+                                           root_transform[4],
+                                           root_transform[5],
+                                           root_transform[:3])
+        elif len(root_transform) == 7:
+            root_transform = gm.frame3_rpy(*root_transform)
+        else:
+            print('root_transform needs to encode transform eiter as xyzrpy, or as xyzqxqyqzqw')
+            exit(1)
 
-	create_nobilia_shelf(km, Path('nobilia'), gm.translation3(2.0, 0, 0))
+    km = GeometryModel()
 
-	km.clean_structure()
-	km.dispatch_events()
+    create_nobilia_shelf(km, Path('nobilia'), root_transform)
 
-	sim = KineverseKinematicSim(km, Path('nobilia'), '/nobilia_description')
+    km.clean_structure()
+    km.dispatch_events()
 
-	print(f'Running kinematics sim for nobilia shelf')
+    sim = KineverseKinematicSim(km, Path('nobilia'), '/nobilia_description', command_type=ValueMapMsg)
 
-	while not rospy.is_shutdown():
-		rospy.sleep(1)
+    print(f'Running kinematics sim for nobilia shelf')
+
+    while not rospy.is_shutdown():
+        rospy.sleep(1)
