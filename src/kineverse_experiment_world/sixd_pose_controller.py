725e924dbf7aba7bf598bc502b80ea73a479a56b
Adrian Röfer <aroefer@cs.uni-freiburg.de>
2022-12-04 02:57:03 +0100

    Removed faulty orientation control from 6D-controller

--- src/kineverse_experiment_world/sixd_pose_controller.py
+++ src/kineverse_experiment_world/sixd_pose_controller.py
@@ -12,20 +12,45 @@ class SixDPoseController(object):
                        goal_pose,
                        controlled_symbols,
                        weight_override=None,
+                       pos_approach_margin=0.05,
                        draw_fn=None):
-        
+
+        # goal_pose = gm.dot(goal_pose, gm.translation3(0, 0, -0.1))
+
         eef = actuated_pose
-        goal_pos_error = gm.norm(gm.pos_of(eef) - gm.pos_of(goal_pose))
         self.eef_rot = eef[:3, :3]
         self.goal_rot = goal_pose[:3, :3]
         self.rot_error_matrix = self.eef_rot - self.goal_rot
         goal_rot_error = gm.norm(self.rot_error_matrix)
+        goal_pos_delta = gm.pos_of(eef) - gm.pos_of(goal_pose)
+        goal_pos_error = gm.norm(goal_pos_delta)
 
-        rot_align_scale = gm.exp(-3 * goal_rot_error) 
+        # proj_tangent = gm.dot(gm.z_of(goal_pose).T, gm.z_of(goal_pose))
 
-        goal_constraints = {'align position': SC(-goal_pos_error * rot_align_scale,
-                                                 -goal_pos_error * rot_align_scale, 10, goal_pos_error),
-                            'align rotation': SC(-goal_rot_error, -goal_rot_error, 1, goal_rot_error)}
+        # pos_tangent  = gm.dot(proj_tangent, goal_pos_delta)
+        # pos_tangent_error = gm.norm(pos_tangent)
+        # pos_normal_error  = gm.abs(gm.dot_product(-gm.z_of(goal_pose), goal_pos_delta))
+
+        # rot_align_scale    = gm.exp(-3 * goal_rot_error) 
+        # approach_ready     = (gm.exp(-pos_tangent_error) ** 2) * rot_align_scale
+        # approach_not_ready = 1 - approach_ready
+
+        rot_x_dot = gm.dot_product(gm.x_of(actuated_pose), gm.x_of(goal_pose))
+        rot_y_dot = gm.dot_product(gm.y_of(actuated_pose), gm.y_of(goal_pose))
+        rot_z_dot = gm.dot_product(gm.z_of(actuated_pose), gm.z_of(goal_pose))
+
+        goal_constraints = {
+                            'align position': SC(-goal_pos_error,
+                                                 -goal_pos_error, 1, goal_pos_error),
+                            'align rot x': SC(1 - rot_x_dot, 1 - rot_x_dot, 1, rot_x_dot),
+                            'align rot y': SC(1 - rot_y_dot, 1 - rot_y_dot, 1, rot_y_dot),
+                            'align rot z': SC(1 - rot_z_dot, 1 - rot_z_dot, 1, rot_z_dot),
+                            # 'align position approach': SC(-pos_normal_error + pos_approach_margin * approach_not_ready,
+                            #                               -pos_normal_error + approach_not_ready, 10, pos_normal_error),
+                            # 'align position tangent': SC(-pos_tangent_error,
+                            #                              -pos_tangent_error, 10, pos_tangent_error),
+                            # 'align rotation': SC(-goal_rot_error, -goal_rot_error, 1, goal_rot_error)
+                            }
 
         self.goal_rot_error = goal_rot_error
 
